534aafbc7c8fa5503e86ed3de8ec828ebbcf7cc5
jos3duardo <jos3duardolopes@gmail.com>
2020-10-14 22:43:10 -0400

    finalizando backend

--- src/controller/OrphanageController.ts
+++ src/controller/OrphanageController.ts
@@ -30,7 +30,7 @@ export default {
       about,
       instructions,
       opening_hours,
-      open_on_weekends,
+	    open_on_weekends,
     } = req.body
 
     const orphanagesRepository = getRepository(Orphanage)
@@ -48,7 +48,7 @@ export default {
       about,
       instructions,
       opening_hours,
-      open_on_weekends,
+	    open_on_weekends: open_on_weekends === 'true',
       images
     }
 
@@ -59,7 +59,7 @@ export default {
       about: Yup.string().required().max(300),
       instructions: Yup.string().required(),
       opening_hours: Yup.string().required(),
-      open_on_weekends: Yup.boolean().required(),
+	    open_on_weekends: Yup.boolean().required(),
       images: Yup.array(
         Yup.object().shape({
           path: Yup.string().required()
